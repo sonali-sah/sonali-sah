8744d097623439605a3bbc285fabfc2bf06b4dd9
sonali-sah <Id>
2024-10-22 01:11:41 +0530

    new commits

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// var menuIcon = document.querySelector(".menu-icon ");
+// var sidebar = document.querySelector(".sidebar");
+
+// menuIcon.onclick = function(){
+//     sidebar.classList.toggle("small-sidebar");
+// } 
+
+var menuIcon = document.querySelector(".menu-icon ");
+var sidebar = document.querySelector(".side-bar");
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("active");
+}
